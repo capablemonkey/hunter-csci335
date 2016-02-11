10cc47076ef7466c2f5b90f940981b0ef3e70ea9
Gordon Zheng <gordon@greenhouse.io>
2016-02-11 15:11:37 -0500

    Fix bug in fixNodeAt

--- assignment-01/heap.cpp
+++ assignment-01/heap.cpp
@@ -31,21 +31,6 @@ public:
   void print();
 };
 
-// template<typename T>
-// int Heap<T>::findIndex(T item, int currentIndex) {
-//   cout << "inspecting index " << currentIndex << endl;
-
-//   if (currentIndex >= list.size()) {
-//     return 0;
-//   }
-
-//   if (list[currentIndex] == item) {
-//     return currentIndex;
-//   }
-
-//   return findIndex(item, indexOfLeftChild(currentIndex)) + findIndex(item, indexOfRightChild(currentIndex));
-// }
-
 // Sequentially seek for the element.  O(n) time.
 template<typename T>
 int Heap<T>::findIndex(T item) {
@@ -71,27 +56,41 @@ void Heap<T>::remove(T item) {
   return;
 }
 
-// Given the index of a node whose position in the heap is invalid, fix the heap by moving the node
-// to its correct position.  If a child node is greater than the node in question and the other child node,
-// swap them.  Reapeat this recursively until the node in question is greater than both of its children.
+// Given the index of a node whose position in the heap is invalid, fix the 
+// heap by moving the node to its correct position.  If a child node is greater 
+// than the node in question and the other child node, swap them.  Reapeat this 
+// recursively until the node in question is greater than both of its children.
 template<typename T>
 void Heap<T>::fixNodeAt(int i) {
+  bool leftChildWithinBounds = indexOfLeftChild(i) < list.size();
+  bool rightChildWithinBounds = indexOfRightChild(i) < list.size();
+  bool leftChildIsMaxChild = (leftChildWithinBounds && rightChildWithinBounds) ? list[indexOfLeftChild(i)] > list[indexOfRightChild(i)] : true;
+  bool rightChildIsMaxChild = (rightChildWithinBounds) ? list[indexOfRightChild(i)] > list[indexOfLeftChild(i)] : true;
+  bool leftChildGreaterThanNode = leftChildWithinBounds ? list[indexOfLeftChild(i)] > list[i] : false;
+  bool rightChildGreaterThanNode = rightChildWithinBounds ? list[indexOfRightChild(i)] > list[i] : false;
+
   // if left child is within bounds and left child is larger than current and the left child is greater than the right child
-  if (indexOfLeftChild(i) < list.size() && 
-      list[indexOfLeftChild(i)] > list[i] && 
-      list[indexOfLeftChild(i)] > list[indexOfRightChild(i)]) { 
+  if (leftChildGreaterThanNode && leftChildIsMaxChild) {
     swapNodes(i, indexOfLeftChild(i));
     return fixNodeAt(indexOfLeftChild(i));
   }
 
-  if (indexOfRightChild(i) < list.size() && 
-      list[indexOfRightChild(i)] > list[i] && 
-      list[indexOfRightChild(i)] > list[indexOfLeftChild(i)]) { 
+  if (rightChildGreaterThanNode && rightChildIsMaxChild) {
     swapNodes(i, indexOfRightChild(i));
     return fixNodeAt(indexOfRightChild(i));
   }
 
   return;
+}
+
+template<typename T>
+void Heap<T>::print() {
+  typedef typename vector<T>::iterator iterator;
+  for (iterator it = list.begin(); it != list.end(); ++it) {
+    cout << " " << *it;
+  }
+
+  cout << endl;
 }
 
 template<typename T>
@@ -107,16 +106,6 @@ void Heap<T>::insert(T item) {
 
   return;
 };
-
-template<typename T>
-void Heap<T>::print() {
-  typedef typename vector<T>::iterator iterator;
-  for (iterator it = list.begin(); it != list.end(); ++it) {
-    cout << " " << *it;
-  }
-
-  cout << endl;
-}
 
 // linear search through leaf nodes for smallest.
 template<typename T>
@@ -149,21 +138,21 @@ void Heap<T>::swapNodes(int a, int b) {
 
 template<typename T>
 int Heap<T>::indexOfParent(int i) {
-  return floor((i - ((i+1) % 2)) / 2.0);
-}
-
-template<typename T>
-int Heap<T>::indexOfRightChild(int i) {
-  if (i == 0) { return 2; }
-
-  return (i * 2) + 1;
+  return floor((i % 2 == 0 ? i - 1 : i) / 2.0);
 }
 
 template<typename T>
 int Heap<T>::indexOfLeftChild(int i) {
   if (i == 0) { return 1; }
 
-  return (i * 2);
+  return (i * 2) + 1;
+}
+
+template<typename T>
+int Heap<T>::indexOfRightChild(int i) {
+  if (i == 0) { return 2; }
+
+  return (i * 2) + 2; 
 }
 
 int main() {
@@ -174,7 +163,8 @@ int main() {
   int element;
   string input;
 
-  // skip first line; we don't need to know length of input since we rely on failbit
+  // skip first line; we don't need to know length of input since we rely
+  // on failbit
   getline(cin, input);
 
   while(getline(cin, input)) {
@@ -182,12 +172,17 @@ int main() {
     ss >> operation;
     ss >> element;
 
+    cout << "=== heap print ";
+    heap.print();
+
     if (operation == 1) {
+      cout << "[ADD] " << element << endl;
       heap.insert(element);
     } else if (operation == 2) {
+      cout << "[REMOVE] " << element << endl;
       heap.remove(element);
     } else {
-      cout << heap.getMinimum() << endl;
+      cout << "[MIN] = " << heap.getMinimum() << endl;
     }
 
   }
