064ab9a95b51a2ed6b5f7491918c5015ab0ccf74
Gordon Zheng <gordon@greenhouse.io>
2016-03-31 13:53:32 -0400

    Add remaining functionality for contacts manager

--- assignment-02/contacts_manager.cpp
+++ assignment-02/contacts_manager.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <exception>
 
 using namespace std;
 
@@ -14,6 +15,10 @@ public:
     this->phone_no = phone_no;
     this->email = email;
   }
+
+  void print() {
+    cout << first_name << ": " << last_name << " | "  << phone_no << " | " << email << endl;
+  }
 };
 
 class ContactManager {
@@ -24,7 +29,7 @@ public:
   void add_new_contact(Contact contact);
   Contact* retrieve_contact(string first_name);
   bool delete_contact(string first_name);
-  void update_contact(string first_name, Contact contact);
+  bool update_contact(string first_name, string last_name, string phone_no, string email);
 };
 
 void ContactManager::add_new_contact(Contact contact) {
@@ -32,11 +37,13 @@ void ContactManager::add_new_contact(Contact contact) {
   // TODO: how to determine it was successful?
 }
 
+class ContactNotFoundException : public exception { };
+
 Contact* ContactManager::retrieve_contact(string first_name) {
   unordered_map<string, Contact>::iterator result = hash_table.find(first_name);
 
   // If not found, find() will return a iterator pointing to the end of the container:
-  if (result == hash_table.end()) { throw "Could not find contact"; }
+  if (result == hash_table.end()) { throw ContactNotFoundException(); }
 
   // Return pointer to the value of the result <key, value> pair
   return &get<1>(*result);
@@ -44,20 +51,54 @@ Contact* ContactManager::retrieve_contact(string first_name) {
 
 bool ContactManager::delete_contact(string first_name) {
   int removed_count = hash_table.erase(first_name);
-
   return removed_count > 0;
+}
+
+bool ContactManager::update_contact(string first_name, string last_name, string phone_no, string email) {
+  try {
+    Contact *original = retrieve_contact(first_name);
+    original->last_name = last_name;
+    original->phone_no = phone_no;
+    original->email = email;
+    return true;
+  } catch (const ContactNotFoundException) { }
+
+  return false;
 }
 
 int main() {
   ContactManager contact_manager;
   Contact bob = Contact("Bob", "McBobson", "3473334444", "bob@bob.com");
+  Contact tom = Contact("Tom", "Jaster", "6462228888", "tom@tom.com");
 
+  // add contacts:
   contact_manager.add_new_contact(bob);
-  contact_manager.delete_contact("Tom");
+  contact_manager.add_new_contact(tom);
 
+  // retrieve contact:
   Contact* result = contact_manager.retrieve_contact("Bob");
+  result->print();
 
-  cout << (*result).first_name << endl;
+  // update contact:
+  contact_manager.update_contact("Bob", "Samuels", "9991112222", "bob@samuels.com");
+  result = contact_manager.retrieve_contact("Bob");
+  result->print();
+
+  // delete contact:
+  bool deleted = contact_manager.delete_contact("Tom");
+  if (deleted) {
+    cout << "PASS: Tom was deleted" << endl;
+  } else {
+    cout << "FAIL: Tom didn't get deleted" << endl;
+  }
+
+  deleted = contact_manager.delete_contact("Roger");
+  if (deleted) { 
+    cout << "FAIL: Delete somehow worked" << endl; 
+  } else { 
+    cout << "PASS: Could not delete non-existent Roger" << endl; 
+  }
+
   return 0;
 }
 
